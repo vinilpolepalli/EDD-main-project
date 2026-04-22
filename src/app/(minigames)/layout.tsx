1184b4d459e8b7cf9a539325adf00cb48b6d1cc6
Vinil Polepalli <vinil.polepalli@gmail.com>
2026-04-22 08:39:01 -0400

    Add sidebar to minigames layout

--- src/app/(minigames)/layout.tsx
+++ src/app/(minigames)/layout.tsx
@@ -1,7 +1,16 @@
+import { Sidebar } from "@/components/shared/sidebar";
+
 export default function MinigamesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="flex min-h-screen bg-background">
+      <Sidebar />
+      <main className="flex-1 pb-20 lg:ml-64 lg:pb-0">
+        {children}
+      </main>
+    </div>
+  );
 }
